9f948a335c4dce3b8c7635ef6f8907ccd87fb49f
Vladimir <32768348+Fronun@users.noreply.github.com>
2018-10-15 20:30:52 +0500

    Опа.

--- Info/main.lua
+++ Info/main.lua
@@ -4,16 +4,19 @@ local GUI = require("GUI")
 local MineOSInterface = require("MineOSInterface")
 local srl = require("serialization") 
 
+local Author = ("Fronun, MineCR")
+
 local version = "1.01"
 
 -- окно
 local mainContainer, window = MineOSInterface.addWindow(GUI.titledWindow(50, 30, 70, 27, "Info", true))
+window.backgroundPanel.colors.transparency = 0.2
 window:addChild(GUI.label(27, 25, window.width, window.height, 0x5A5A5A, "Version: "..version))
---window:addChild(GUI.label(2, 27, window.width, window.height, 0x5A5A5A, "Автор: Fronun ")) 
-window:addChild(GUI.label(25, 2, window.width, window.height, 0x5A5A5A, "Информация о компьютере:"))
+window:addChild(GUI.label(3, 27, window.width, window.height, 0x5A5A5A, Author)) 
+window:addChild(GUI.label(25, 2, window.width, window.height, 0x5A5A5A, "Information about the computer:"))
 
 
---component.list([filter:string]):function
+
 
 listCom = ""
 posYcom = 4
@@ -25,5 +28,5 @@ window:addChild(GUI.label(26, posYcom, window.width, window.height, 0x5A5A5A, in
 posYcom = posYcom+1
 end
 --print (listCom)
---window:addChild(GUI.label(5, 4, window.width, window.height, 0x5A5A5A, listCom ))
+window:addChild(GUI.label(5, 4, window.width, window.height, 0x5A5A5A, listCom ))
 --window:addChild(GUI.label(5, 5, window.width, window.height, 0x5A5A5A,  ))
